5672967fbb3493d04de3c49f6dbbd0d315f5f2c8
linfuxiang <linfuxiang18@163.com>
2016-12-27 00:09:21 +0800

    add new modules

--- createModel.js
+++ createModel.js
@@ -3,17 +3,22 @@ const mongoose = require('./connectDB');
  * 数据库字段描述
  * @tS {Object} 数据库对象集合
  * @city {String} 城市名
- * @num {String} 空气质量指数
+ * @num {Number} 空气质量指数
  * @situ {String} 空气质量状况
  * @collectionName {String} 数据库集合（表）名
  */
 let tS = new mongoose.Schema({
     city: { type: String },
-    num: { type: String },
+    num: { type: Number },
     situ: { type: String }
 });
 let today = new Date();
-let collectionName = 'spidertest-' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '_' + today.getHours() + ':00';
-// let collectionName = 'spidertest-' + parseInt(Math.random()*100);
-// let user = 
-module.exports = mongoose.model(collectionName, tS);
+// let collectionName = 'spidertest-' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '_' + today.getHours() + ':00';
+let createModel = function(collectionParams) {
+    let collectionName = 'spidertest-' + collectionParams;
+    return mongoose.model(collectionName, tS);
+}
+exports.model = createModel;
+// exports.model = mongoose.model(collectionName, tS);
+// exports.time = collectionName;
+exports.connection = mongoose.connection;
--- spider.js
+++ spider.js
@@ -1,54 +1,53 @@
-// const http = require('http');
-// const express = require('express');
-// const mongoose = require('mongoose');
 const charset = require('superagent-charset');
 const superagent = charset(require('superagent'));
-const user = require('./createModel');
+const user1 = require('./createModel');
 const cheerio = require('cheerio');
-// let app = express();
 
-// const uri = 'mongodb://localhost:10001/test';
-
-// app.get('/', function(req, res){
-// 用 superagent 去抓取 https://cnodejs.org/ 的内容
+// 用 superagent 去抓取内容
 superagent.get('http://www.tianqi.com/air/')
     .charset('gbk')
     .end(function(err, sres) {
         if (err) {
             return next(err);
         }
-        // let db = mongoose.connection;
-        // db.on('error', function() {
-        //     console.error('error.');
-        // });
-        // db.once('connected', function() {
-        //     console.log('open.');
-        // });
-        // db.on('disconnected', function() {
-        //     console.log('disconnected.');
-        // });
-        // mongoose.Promise = global.Promise;
-        // mongoose.connect(uri);
-        // let tS = new mongoose.Schema({
-        //     city: { type: String },
-        //     num: { type: String },
-        //     situ: { type: String }
-        // });
-
-        // let today = new Date();
-        // let collectionName = 'spidertest-' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate());
-        // let user = mongoose.model(collectionName, tS);
+        let $ = cheerio.load(sres.text);
+        let cityList = $('.meta li');
+        /* 更新表 */
+        cityList.each(function(idx, element) {
+            if (idx == 0) {
+                return;
+            }
+            let $element = $(element);
+            let city = $element.find('span').eq(1).find('a').text();
+            let num = $element.find('span').eq(2).text();
+            let situ = $element.find('span').eq(3).find('em').text();
+            let where = {
+                city: city
+            };
+            let upd = {
+                num: num,
+                situ: situ
+            }
+            let userlatest = user1.model('latest');
+            userlatest.update(where, upd, function(err, res) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(idx + 'update success');
+                }
+            });
+        });
+        /* 存表 */
+        let today = new Date();
+        let collectionName = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '_' + today.getHours() + ':00';
+        let user = user1.model(collectionName);
         user.count({}, function(err, res) {
             if (err) {
                 console.log("Error:" + err);
             } else {
                 if (res == 0) {
-                    let $ = cheerio.load(sres.text);
-                    let cityList = $('.meta li');
                     let len = cityList.length - 1,
                         count = 0;
-                    // 需要重新连接一次数据库
-                    // mongoose.connect(uri);
                     cityList.each(function(idx, element) {
                         if (idx == 0) {
                             return;
@@ -64,25 +63,21 @@ superagent.get('http://www.tianqi.com/air/')
                         };
                         (new user(jsons)).save(function(err, res) {
                             if (err) {
-                                console.error(err);
+                                console.error(idx + err);
                             } else {
-                                console.log('success');
+                                console.log(idx + 'save success');
                                 count++;
                                 if (count === len) {
                                     console.log('finished');
+                                    // user1.connection.close();
                                 }
                             }
                         });
                     });
                 } else {
-                    console.log('今天的数据已抓取.');
+                    console.log(collectionName + '的数据已抓取.');
+                    // user1.connection.close();
                 }
             }
-            // db.close();
         });
-        // db.close();
     });
-// });
-
-// app.listen('8088', ()=>{});
-
