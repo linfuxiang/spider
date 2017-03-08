29e219cb2c9a22d9857dca18bada682f11e75b4a
linfuxiang <linfuxiang18@163.com>
2017-03-08 18:37:40 +0800

    fix GET bug

--- api.js
+++ api.js
@@ -24,7 +24,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     // res.header('Access-Control-Allow-Origin', 'https://www.superlfx.cn');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'POST, GET');
     res.header("X-Powered-By", '3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
     // if (req.method == 'OPTIONS') {
@@ -108,6 +108,7 @@ app.post('/signin', (req, res) => {
         // pw = params.query.pw;
     un = req.body.un,
     pw = req.body.pw;
+    console.log(un, pw);
     user.find({ un: un, pw: pw }).exec(function(err, r) {
         if (err) {
             console.log("Error:" + err.code);
